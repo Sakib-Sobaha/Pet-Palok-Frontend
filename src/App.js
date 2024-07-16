ee878f343f28ec9cba763969114ba60f451085c1
Sakib Mohammed Sobaha <1905021@ugrad.cse.buet.ac.bd>
2024-07-16 23:49:17 +0600

    theme night added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,40 @@ import "./App.css";
 export default function App() {
   return (
     <div>
+      <label className="flex cursor-pointer gap-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <circle cx="12" cy="12" r="5" />
+          <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+        </svg>
+        <input
+          type="checkbox"
+          value="night"
+          className="toggle theme-controller"
+        />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+        </svg>
+      </label>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
 
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
@@ -14,6 +48,8 @@ export default function App() {
       <button className="btn btn-success">Success</button>
       <button className="btn btn-warning">Warning</button>
       <button className="btn btn-secondary">Error</button>
+
+      <button className="btn glass">Glass button</button>
 
       {/* You can open the modal using document.getElementById('ID').showModal() method */}
       <button
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <html data-theme="dark"></html>
   </React.StrictMode>
 );
 
